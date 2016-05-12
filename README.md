acf84a6f544d3af8f47d2567e768404b61c0b1a9
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-05-12 12:09:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# URL to Markdown
+
+[Download](https://raw.github.com/yuku-t/alfred-url-to-markdown-workflow/master/url-to-markdown.alfredworkflow)
---
+++ src/link.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'uri'
+
+class Extracter
+  TITLE_REGEXP = %r{<title>(?<title>.*)</title>}m
+  GITHUB_REGEXP = %r{https://github.com/(?<repo>[^/]+/[^/]+)/(?:issues|pull)/(?<id>\d+)(?<path>/.*)?}
+
+  # @param url [String]
+  def initialize(url)
+    @url = url
+  end
+
+  # @return [true, false]
+  def extracted?
+    !title.nil?
+  end
+
+  # @return [String, nil]
+  def markdown_title
+    "[#{extracted? ? title : 'Unknown'}](#{@url})"
+  end
+
+  # @return [String]
+  def title
+    if match = GITHUB_REGEXP.match(@url)
+      "#{match[:repo]}##{match[:id]}#{match[:path]}"
+    elsif match = TITLE_REGEXP.match(html)
+      match[:title].gsub("\n", '')
+    else
+      nil
+    end
+  end
+
+  private
+
+  # @return [String]
+  def html
+    @html ||= fetch(@url)
+  end
+
+  # @param url_string [String]
+  # @param limit [Integer]
+  # @return [String]
+  def fetch(url_string, limit = 10)
+    url = URI.parse(url_string)
+    fail ArgumentError, "Invalid HTTP url: #{url_string}" unless url.is_a?(URI::HTTP)
+    fail ArgumentError, 'HTTP redirect too deep' if limit == 0
+
+    res = Net::HTTP.get_response(url)
+    case res
+    when Net::HTTPSuccess
+      res.body
+    when Net::HTTPRedirection
+      fetch(res['location'], limit - 1)
+    else
+      res.value
+    end
+  end
+end
+
+begin
+  extracter = Extracter.new(ARGV[0])
+  if extracter.extracted?
+    puts extracter.markdown_title
+    `osascript -e 'display notification "#{extracter.title}" with title "link"'`
+  else
+    `osascript -e 'display notification "Cannot extract title" with title "link"'`
+  end
+rescue => e
+  `osascript -e 'display notification "#{e.message}" with title "link"'`
+end
